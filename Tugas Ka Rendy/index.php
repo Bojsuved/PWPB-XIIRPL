aa480f91871c6df3980969f5e6e5642f333c2df3
Wither73 <erlanasap73@gmail.com>
2022-11-15 13:58:26 +0700

    Tugas Ka Rendy

---
+++ Tugas Ka Rendy/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CSS 3 - Border Radius</title>
+    <link rel="stylesheet" href="style.css">
+    <style>
+        .kotak {
+            display: inline-block;
+            width: 150px;
+            height: 150px;
+            border: 10px solid black;
+            border-radius: 10px;
+            background-color: white;
+            border-color: whitesmoke;
+            margin: 20px;
+        }
+    </style>
+</head>
+<body>
+    <div class="kotak"></div>
+    <div class="kotak2"></div>
+    <div class="kotak3"></div>
+    <div class="kotak4"></div>
+    <div class="kotak5"></div>
+    <div class="kotak6"></div>
+</body>
+</html>
